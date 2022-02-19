3399e212a88b274865eaa44588b5500a8c06952a
i-Taozi <879714748@qq.com>
2022-02-19 23:45:51 +0800

    ligasgr/intellij-xquery:original code

--- src/main/resources/googlehtml5.groovy
+++
@@ -1,9 +0,0 @@
-// profile for html5slides (http://html5slides.googlecode.com/)
-
-isLastSlide= {
-    js 'curSlide == slideEls.length-1'
-}
-
-nextSlide= {
-    js 'nextSlide();'
-}
--- src/main/resources/remarkjs.groovy
+++
@@ -1,7 +0,0 @@
-isLastSlide = {
-    js 'window.slideshow.getCurrentSlideIndex() >= window.slideshow.getSlideCount() - 1;'
-}
-
-nextSlide = {
-    js 'window.slideshow.gotoNextSlide();'
-}
